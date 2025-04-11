96722b3f4b1857a7ddb6fed842f4221fd24998aa
Ali <ali.program1402@gmail.com>
2025-04-12 03:02:45 +0800

    Modification of the Database class

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,39 +1,57 @@
 package db;
 import db.exception.EntityNotFoundException;
+import db.exception.InvalidEntityException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
-    private static final ArrayList<Entity> entities = new ArrayList<>();
-    private static int nextId = 1;
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    private static int Id = 1;
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
 
-    private Database() {}
-
-    public static void add(Entity e) {
-        Entity copy = e.copy();
-        copy.id = nextId;
-        e.id = nextId++;
-        entities.add(copy);
+    public static void setValidators(HashMap<Integer, Validator> validators) {
+        Database.validators = validators;
     }
 
-    public static Entity get(int id) throws EntityNotFoundException {
-        for (Entity e : entities) {
-            if (e.id == id) {
+    public static void add(Entity e) throws InvalidEntityException {
+        Validator validator = validators.get(e.getEntityCode());
+        if (validator != null) {
+            validator.validate(e);
+        }
+        e.id = Id++;
+        entities.add(e.copy());
+    }
+
+    public static Entity get(int id) {
+        for(Entity e: entities){
+            if(e.id == id){
                 return e.copy();
             }
         }
         throw new EntityNotFoundException(id);
     }
-
-    public static void delete(int id) throws EntityNotFoundException {
-        boolean removed = entities.removeIf(e -> e.id == id);
-        if (!removed) {
-            throw new EntityNotFoundException(id);
+    public static void delete(int id)  {
+        for(Entity e: entities){
+            if(e.id == id){
+                entities.remove(e);
+                return;
+            }
         }
+        throw new EntityNotFoundException(id);
     }
+    public static void update(Entity e) throws InvalidEntityException {
+        Validator validator = validators.get(e.getEntityCode());
+        if (validator != null) {
+            validator.validate(e);
+        }
+        Entity existing = get(e.id);
+        entities.set(entities.indexOf(existing), e.copy());
+    }
+    public static void registerValidator(int entityCode, Validator validator) {
+        if(validators.containsKey(entityCode)){
+            throw new IllegalArgumentException("Entity with code " + entityCode + " already exists");
+        }
+        validators.put(entityCode, validator);
 
-public static void update(Entity e) throws EntityNotFoundException {
-    delete(e.id);
-    Entity updatedCopy = e.copy();
-    entities.add(updatedCopy);
     }
 }
